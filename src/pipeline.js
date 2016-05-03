952f3b7c17b1586880f013e4c43342b01c381177
Emanuele Busi <emanuele.busi@mondora.com>
2016-05-03 11:17:23 +0200

    Fixed "Mod on _id not allowed" error

--- src/pipeline.js
+++ src/pipeline.js
@@ -5,7 +5,9 @@ import {ACTION_INSERT, ACTION_UPDATE, ACTION_DELETE} from "./config";
 export default async function pipeline (event, action) {
     var sensor = event.data.element || {};
     const id = event.data.id;
-    sensor._id = id;
+    if (!id) {
+        return null;
+    }
     sensor.isDeleted = action === ACTION_DELETE;
 
     if (sensor.formula) {
@@ -17,14 +19,14 @@ export default async function pipeline (event, action) {
     switch (action) {
     case ACTION_INSERT:
         sensor.createdDate = now;
-        await insert(sensor);
+        await insert(sensor, id);
         break;
     case ACTION_UPDATE:
         sensor.lastModifiedDate = now;
-        await update(sensor);
+        await update(sensor, id);
         break;
     case ACTION_DELETE:
-        await logicalDelete(sensor);
+        await logicalDelete(id);
         break;
     }
 
--- src/services/mongodb.js
+++ src/services/mongodb.js
@@ -7,25 +7,26 @@ import {
 
 export default MongoClient.connect(MONGODB_URL);
 
-export async function insert (sensor) {
+export async function insert (sensor, id) {
     const db = await MongoClient.connect(MONGODB_URL);
+    sensor._id = id;
     return db.collection(SENSORS_COLLECTION_NAME).insert(
         sensor
     );
 }
 
-export async function update (sensor) {
+export async function update (sensor, id) {
     const db = await MongoClient.connect(MONGODB_URL);
     return db.collection(SENSORS_COLLECTION_NAME).update(
-        {_id: sensor._id},
+        {_id: id},
         {$set: sensor}
     );
 }
 
-export async function logicalDelete (sensor) {
+export async function logicalDelete (id) {
     const db = await MongoClient.connect(MONGODB_URL);
     return db.collection(SENSORS_COLLECTION_NAME).update(
-        {_id: sensor._id},
+        {_id: id},
         {$set: {isDeleted: true}}
     );
 }
